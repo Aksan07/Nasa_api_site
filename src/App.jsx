102310f07cc5a7f306ec22519a82b2fe47157343
Aksan07 <aksanpramod1@gmail.com>
2024-10-09 14:36:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Main from "./components/Main";
+import SideBar from "./components/SideBar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Main />
+      <SideBar></SideBar>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
